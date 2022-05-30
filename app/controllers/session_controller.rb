3a5015603df4aa03ac3911f3e22e2b562ee484ca
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2022-05-30 17:40:53 -0300

    Styled sign and log in

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,6 +3,15 @@ class SessionController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:email])
+
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash[:alert] = "Invalid mail or password"
+            render :new
+        end
     end
 
     def destroy
