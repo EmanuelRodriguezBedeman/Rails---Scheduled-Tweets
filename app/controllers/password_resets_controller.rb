72f9b3612f8116ea8a227e5dc317b28ab7041b97
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2022-06-03 16:47:38 -0300

    Added Forgot Passward and reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,13 +3,32 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
-        @user = user.find_by(email: params[:email])
+        @user = User.find_by(email: params[:email])
 
         if @user.present?
             # Send email
-            PasswordMailer.with(user: @user).reset.deliver_later
+            PasswordMailer.with(user: @user).reset.deliver_now
         end
 
         redirect_to root_path, notice: "If an account with that email was found, we have sent to reset your password."
+    end
+
+    def edit
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to log_in_path, alert: "Your token has expired. Please try again."
+    end
+
+    def update
+        @user = User.find_signed(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to log_in_path, alert: "Your password was reset successfully. Please log in."
+        else
+            rendir :edit
+        end
+    end
+
+    def password_params
+        params.require(user).permit(:password, :password_confirmation)
     end
 end
