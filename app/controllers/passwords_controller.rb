fa04eee55af6f266840b6554d216a25ade04c889
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2022-06-30 15:35:57 -0300

    fixed form frontend error

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -17,6 +17,6 @@ class PasswordsController < ApplicationController
     private 
 
     def password_params
-        params.require.(:user).permit(:password, :password_confirmation)
+        params.require.(user).permit(:password, :password_confirmation)
     end
 end
