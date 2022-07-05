9a05f81028fca33bcc45c6460cc79af25794be8f
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2022-07-05 19:13:07 -0300

    Fixed password change bug

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,34 +2,21 @@
 
 class PasswordsController < ApplicationController
     before_action :require_user_logged_in!
-    before_action :password_params, only: [:update]
 
     def edit
     end
 
     def update
-
-        puts("Hello from update!")
-
         if Current.user.update(password_params)
             redirect_to root_path, notice: "Password updated!"
         else
-            render :edit
+            render :edit, notice: "Password error."
         end
     end
 
     private
 
     def password_params
-        puts("Hello from password_params!")
-
-        puts params
-        
-        puts("\n↓↓ Start of params inspect ↓↓ \n\n")
-        
-        puts params.inspect
-
-        puts("\n↑↑ End of params inspect ↑↑ \n\n")
-        params.permit(user: [:password, :repeat_password])
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
