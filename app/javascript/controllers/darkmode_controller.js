eddd8c637193a2f2a0c6dc301d35e398005d33ee
EmanuelRodriguezBedeman <emanuel.rodriguez.bedeman@gmail.com>
2022-11-17 20:13:59 -0300

    Testing Stimulus (2)

--- app/javascript/controllers/darkmode_controller.js
+++ app/javascript/controllers/darkmode_controller.js
@@ -1,7 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    alert("ok")
-  }
+    static targets = ["element", "ball"]
+
+    themeToggle() {
+        this.element.classList.toggle("dark-theme")
+    }
 }
